d05c10580d5b02df66e08beb1103ccfd7fde7646
Ben Weber <dev.ben.weber@gmail.com>
2020-08-20 14:38:54 +0200

    Added pyproject.toml and tox

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import setuptools
 
-
-setup(
-    name="dusk",
-    version="0.5.0",
-    packages=find_packages(),
-    author="MeteoSwiss",
-    author_email="Benjamin.Weber@MeteoSwiss.ch",
-    description="An eDSL front-end for Numerical Weather Prediction dynamical cores",
-    keywords="dawn SIR eDSL DSL NWP dycore ICON",
-    url="https://github.com/dawn-ico/dusk",
-    license="MIT License",
-    python_requires=">=3.8",
-    install_requires=["dawn4py>=0.0.2"],
-    entry_points={"console_scripts": ["dusk = dusk.cli:main"]},
-)
+# setup.py shim so that pip can do editable installs (`pip install -e .`)
+if __name__ == "__main__":
+    setuptools.setup()

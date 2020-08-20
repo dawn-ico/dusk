414d907ecf8d98fbccbe15b8df15bd5c0b6b84d5
Ben Weber <dev.ben.weber@gmail.com>
2020-08-20 17:07:55 +0200

    Added a few more basic tests

---
+++ tests/conftest.py
@@ -0,0 +1,2 @@
+# this empty file makes that `pytest` will add this folder to `PYTHONPATH`,
+# then the tests can import `test_util.py`
---
+++ tests/stencils/test_math.py
@@ -0,0 +1,35 @@
+from dusk.script import *
+from test_util import transpile_and_validate
+
+
+def test_math():
+    transpile_and_validate(math_stencil)
+
+
+@stencil
+def math_stencil(a: Field[Cell], b: Field[Cell], c: Field[Cell], d: Field[Cell]):
+
+    with levels_upward:
+
+        a = sqrt(b)
+        b = exp(c)
+        c = log(a)
+        a = sin(b)
+        b = cos(c)
+        c = tan(a)
+        a = arcsin(b)
+        b = arccos(c)
+        c = arctan(a)
+        a = fabs(b)
+        b = floor(c)
+        c = ceil(a)
+        a = isinf(b)
+        b = isnan(c)
+
+        a = max(b, c)
+        d = min(a, b)
+        c = pow(d, a)
+
+        a = a + sqrt(b) + cos(c)
+        a = max(min(b, c), d)
+

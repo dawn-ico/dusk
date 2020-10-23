b442b97ad379c5f7aba45c502d25d66b97183515
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-10-23 09:53:28 +0200

    Address review feedback

--- dusk/cli.py
+++ dusk/cli.py
@@ -10,8 +10,8 @@ def main() -> None:
         description="Transforms the Python embedded DSL to generated code through Dawn.",
     )
     argparser.add_argument("in_file", type=str, help="Input file (dusk stencil)")
-    argparser.add_argument("out_file", type=str,
-                           help="Output file (generated code)")
+    argparser.add_argument("-o", type=str, dest="out_file",
+                           help="Output file (generated code), default: <base_in_file>_<backend>.cpp")
     argparser.add_argument(
         "-b",
         type=str,
@@ -20,14 +20,19 @@ def main() -> None:
         default=default_backend,
         help="Backend to use to generate code",
     )
-    argparser.add_argument("-dump-sir", default=False, action="store_true", help="Dumps (also) sir to <base_out_file>.json")
+    argparser.add_argument("-dump-sir", default=False, action="store_true", help="Dumps (also) sir to <base_in_file>.json")
     argparser.add_argument("-verbose", dest="verbose", default=False, action="store_true", help="Enables verbosity of dawn",)
 
     args = argparser.parse_args()
+
+    in_filename = path.splitext(path.basename(args.in_file))[0]
+
+    if args.out_file is None:
+        args.out_file = in_filename + "_" + args.backend + ".cpp"
     out_stream = open(args.out_file, "w")
     
     if args.dump_sir:
-        sir_stream = open(path.splitext(args.out_file)[0] + ".json", "w")
+        sir_stream = open(in_filename + ".json", "w")
 
     transpile(args.in_file, 
               sir_stream if args.dump_sir else None,
--- dusk/front.py
+++ dusk/front.py
@@ -10,19 +10,12 @@ def main() -> None:
         description="Transforms the Python embedded DSL to SIR.",
     )
     argparser.add_argument("in_file", type=str, help="Input file (dusk stencil)")
-    argparser.add_argument("-o", type=str, dest="out_file",
-                           help="Write SIR to file instead of standard output")
     
     args = argparser.parse_args()
-    if args.out_file:
-        sir_stream = open(args.out_file, "w")
-    else:
-        sir_stream = stdout
 
-    transpile(args.in_file, sir_stream,
+    transpile(args.in_file, stdout,
               None # don't need to codegenerate
              )
-    sir_stream.close()
 
 
 if __name__ == "__main__":
